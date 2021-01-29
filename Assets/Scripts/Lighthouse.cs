ad91c7f1b91688f9c1c5e90790b7d8cc4a78b76f
Mariana Orihuela <morihuelacazorla@gmail.com>
2021-01-29 18:27:21 +0100

    Added lighthouse rotation

--- Assets/Scripts/Lighthouse.cs
+++ Assets/Scripts/Lighthouse.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Lighthouse : MonoBehaviour
 {
+    [SerializeField] Transform lightPos;
+    Quaternion originalRot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        originalRot = transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(new Vector3(lightPos.position.x, transform.position.y, lightPos.position.z));
     }
 }
