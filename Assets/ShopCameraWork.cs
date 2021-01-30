29fd3510324a699a11eff5f7d86b4cc00491a665
xugarteamundarain <xugarteamundarain@gmail.com>
2021-01-30 17:10:02 +0100

    ShopScene initial commit
    Added ShopScene and ShopMenu prefab

---
+++ Assets/ShopCameraWork.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopCameraWork : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    float velocity;
+
+    void Update()
+    {
+        transform.RotateAround(target.position, Vector3.up, velocity * Time.deltaTime);
+    }
+}
