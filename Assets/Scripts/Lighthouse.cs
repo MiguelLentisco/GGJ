722b52f9aafa9d7d34137437ad5eb6d2adf2c94b
Mariana Orihuela <morihuelacazorla@gmail.com>
2021-01-30 13:07:36 +0100

    Added circle to the lighthouse radius

--- Assets/Scripts/Lighthouse.cs
+++ Assets/Scripts/Lighthouse.cs
@@ -6,11 +6,22 @@ public class Lighthouse : MonoBehaviour
 {
     [SerializeField] Transform lightPos;
     Quaternion originalRot;
+    [SerializeField] float maxDistance = 3f;
+    float minScale = 1.6f;
+    float maxScale = 2.667f;
 
     // Start is called before the first frame update
     void Start()
     {
         originalRot = transform.rotation;
+        
+        lightPos.gameObject.GetComponent<Luz_Faro>().ChangeDistance(maxDistance);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxDistance);
     }
 
     // Update is called once per frame
@@ -18,4 +29,49 @@ public class Lighthouse : MonoBehaviour
     {
         transform.LookAt(new Vector3(lightPos.position.x, transform.position.y, lightPos.position.z));
     }
+
+    void IncreaseRadius()
+    {
+        gameObject.transform.Find("Limit").transform.localScale = new Vector3(2.6667f, 2.6667f, 2.6667f);
+
+        maxDistance = 5f;
+        lightPos.gameObject.GetComponent<Luz_Faro>().ChangeDistance(maxDistance);
+    }
+
+    IEnumerator ScaleUpOverTime(float time)
+    {
+        Vector3 originalScale = new Vector3(minScale, minScale, minScale);
+        Vector3 destinationScale = new Vector3(maxScale, maxScale, maxScale);
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            gameObject.transform.Find("Limit").transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            maxDistance = 5f;
+            lightPos.gameObject.GetComponent<Luz_Faro>().ChangeDistance(maxDistance);
+            yield return null;
+        } while (currentTime <= time);
+
+    }
+
+    IEnumerator ScaleDownOverTime(float time)
+    {
+        Vector3 originalScale = new Vector3(maxScale, maxScale, maxScale);
+        Vector3 destinationScale = new Vector3(minScale, minScale, minScale);
+
+        float currentTime = 0.0f;
+
+        do
+        {
+            gameObject.transform.Find("Limit").transform.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+            currentTime += Time.deltaTime;
+            maxDistance = 3f;
+            lightPos.gameObject.GetComponent<Luz_Faro>().ChangeDistance(maxDistance);
+            yield return null;
+        } while (currentTime <= time);
+
+    }
+
 }
--- Assets/Scripts/Luz_Faro.cs
+++ Assets/Scripts/Luz_Faro.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 public class Luz_Faro : MonoBehaviour
 {
     Vector3 newPosition;
+    [SerializeField] Transform lighthousePos;
+    float maxDistance;
     [SerializeField] float speed = 5.0f;
     
     void Start()
     {
         newPosition = transform.position;
     }
+
+    public void ChangeDistance(float newDistance)
+    {
+        maxDistance = newDistance;
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -32,10 +40,15 @@ public class Luz_Faro : MonoBehaviour
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         if (Physics.Raycast(ray, out hit))
         {
-            newPosition = hit.point;
-            newPosition.y = 4.0f;
+            if (Vector3.Distance(hit.point,lighthousePos.position) < maxDistance)
+            {
+                newPosition = hit.point;
+                newPosition.y = 4.0f;
+            }          
+            
         }
     }
 }
