7a33e1e5bde0941ae08e59a3a2ea46c95ff2126c
xugarteamundarain <xugarteamundarain@gmail.com>
2021-01-30 01:22:45 +0100

    Main Menu, LevelManager & Tags
    Creado nuevo Main Menu básico (junto a ajustes con cambio de volumen, calidad y fullscreen y un efecto pequeño de cámara) y está todo metido dentro del prefab MainMenu
    Creado dos nuevos tags: Obstaculo y Puerto
    Creado un LevelManager que maneje el cambio de niveles

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    string actualNivel;
+
+    private void Start()
+    {
+        // Cargar el primer nivel (el main menu)
+        //SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+    }
+
+    public void cargarNivel(string pNombreNivel)
+    {
+        SceneManager.UnloadSceneAsync(actualNivel);
+        SceneManager.LoadScene(pNombreNivel, LoadSceneMode.Additive);
+        actualNivel = pNombreNivel;
+    }
+
+    public void salirNivel()
+    {
+        Application.Quit();
+    }
+
+    public void ajustarVolumen (float volumen)
+    {
+        Debug.Log(volumen);
+    }
+
+    public void ajustarCalidad(int calidad)
+    {
+        QualitySettings.SetQualityLevel(calidad);
+    }
+
+    public void ajustarFullScreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+}
---
+++ Assets/Scripts/MenuCameraWork.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraWork : MonoBehaviour
+{
+    [SerializeField]
+    Transform menuMain;
+    [SerializeField]
+    Transform menuPosition;
+    [SerializeField]
+    float transitionSpeed;
+    [SerializeField]
+    Canvas menuCanvas;
+    [SerializeField]
+    Canvas optionCanvas;
+
+    // 0 == no change, 1 == goToOptions, 2 == returnToMainMenu
+    int states = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        optionCanvas.gameObject.SetActive(false);
+        menuCanvas.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        if (states != 0) { 
+            if (states == 1)
+            {
+                transform.position = Vector3.Lerp(transform.position, menuPosition.position, Time.deltaTime * transitionSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, menuPosition.rotation, Time.deltaTime * transitionSpeed);
+            } else
+            {
+                transform.position = Vector3.Lerp(transform.position, menuMain.position, Time.deltaTime * transitionSpeed);
+                transform.rotation = Quaternion.Lerp(transform.rotation, menuMain.rotation, Time.deltaTime * transitionSpeed);
+            }
+        }
+    }
+
+    public void goToOptions()
+    {
+        states = 1;
+        menuCanvas.gameObject.SetActive(false);
+        optionCanvas.gameObject.SetActive(true);
+    }
+
+    public void returnToMenu()
+    {
+        states = 2;
+        menuCanvas.gameObject.SetActive(true);
+        optionCanvas.gameObject.SetActive(false);
+    }
+}
