58fc29a823996f63f3498623520c1ce5524ab82b
xugarteamundarain <xugarteamundarain@gmail.com>
2021-01-31 12:24:56 +0100

    Created prefabs for Pause menu and Credits

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject GameOverUI;
+    [SerializeField]
+    GameObject CreditsUI;
+
+    bool finalizar = false;
+
+    private void Start()
+    {
+        GameOverUI.SetActive(true);
+        CreditsUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (!finalizar && Input.GetMouseButtonDown(0))
+        {
+            GameOverUI.SetActive(false);
+            CreditsUI.SetActive(true);
+            StartCoroutine(waitThenFinalizar());
+        }
+
+        if (finalizar && Input.GetMouseButtonDown(0))
+        {
+            GameOverUI.SetActive(false);
+            CreditsUI.SetActive(false);
+            LevelManager.instance.loadLevel("Scene1");
+        }
+    }
+
+    IEnumerator waitThenFinalizar()
+    {
+        yield return new WaitForSeconds(2);
+        finalizar = true;
+    }
+}
